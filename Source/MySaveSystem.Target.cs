e03d74e295ba7251363514cb51ce92c2bc55451f
ArsenyAtman <arsenmail2018@gmail.com>
2025-06-29 14:25:28 +0300

    Migrate to 5.5

--- Source/MySaveSystem.Target.cs
+++ Source/MySaveSystem.Target.cs
@@ -8,7 +8,8 @@ public class MySaveSystemTarget : TargetRules
 	public MySaveSystemTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.AddRange( new string[] { "MySaveSystem" } );
 	}
 }
--- Source/MySaveSystemEditor.Target.cs
+++ Source/MySaveSystemEditor.Target.cs
@@ -8,7 +8,8 @@ public class MySaveSystemEditorTarget : TargetRules
 	public MySaveSystemEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.AddRange( new string[] { "MySaveSystem" } );
 	}
 }
